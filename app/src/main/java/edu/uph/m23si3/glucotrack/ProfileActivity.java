3bd20f09cacabfbb45e8133b715c2660b263051d
angelia3443 <erikaangelia2005@gmail.com>
2025-06-24 12:07:14 +0700

    kedua

--- app/src/main/java/edu/uph/m23si3/glucotrack/ProfileActivity.java
+++ app/src/main/java/edu/uph/m23si3/glucotrack/ProfileActivity.java
@@ -1,9 +1,13 @@
 package edu.uph.m23si3.glucotrack;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -19,6 +23,8 @@ public class ProfileActivity extends AppCompatActivity {
 
     Spinner spinnerSex, spinnerDiabetes;
     Switch switchInsulin;
+    EditText edtNama, edtEmail, edtAge, edtTarget;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,32 +38,69 @@ public class ProfileActivity extends AppCompatActivity {
             return insets;
         });
 
-        // Inisialisasi view
+        // Inisialisasi View
         spinnerSex = findViewById(R.id.spinner_sex);
         spinnerDiabetes = findViewById(R.id.spinner_diabetes);
-        switchInsulin = findViewById(R.id.switch_insulin); // Pastikan ID-nya sama di XML
+        switchInsulin = findViewById(R.id.switch_insulin);
+        edtNama = findViewById(R.id.edtNama);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtAge = findViewById(R.id.edtAge);
+        edtTarget = findViewById(R.id.edtTarget);
 
-        // Data spinner
+        // SharedPreferences
+        prefs = getSharedPreferences("user_profile", MODE_PRIVATE);
+
+        // Load data kalau ada
+        edtNama.setText(prefs.getString("nama", ""));
+        edtEmail.setText(prefs.getString("email", ""));
+        edtAge.setText(prefs.getString("age", ""));
+        edtTarget.setText(prefs.getString("target", ""));
+
+        // Auto save saat mengetik
+        edtNama.addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                prefs.edit().putString("nama", s.toString()).apply();
+            }
+        });
+
+        edtEmail.addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                prefs.edit().putString("email", s.toString()).apply();
+            }
+        });
+
+        edtAge.addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                prefs.edit().putString("age", s.toString()).apply();
+            }
+        });
+
+        edtTarget.addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                prefs.edit().putString("target", s.toString()).apply();
+            }
+        });
+
+        // Data Spinner
         String[] sexOptions = {"Male", "Female", "Other"};
         String[] diabetesTypes = {"Type 1", "Type 2", "Gestational", "Other"};
 
-        // Adapter spinner sex
         ArrayAdapter<String> sexAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sexOptions);
         sexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerSex.setAdapter(sexAdapter);
 
-        // Adapter spinner diabetes
         ArrayAdapter<String> diabetesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, diabetesTypes);
         diabetesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDiabetes.setAdapter(diabetesAdapter);
 
-        // Switch listener
+        // Switch handler
         switchInsulin.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                Toast.makeText(this, "Insulin Therapy: Yes", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Insulin Therapy: No", Toast.LENGTH_SHORT).show();
-            }
+            String msg = isChecked ? "Insulin Therapy: Yes" : "Insulin Therapy: No";
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
         });
     }
 
@@ -72,6 +115,10 @@ public class ProfileActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.mBersihkanForm) {
+            edtNama.setText("");
+            edtEmail.setText("");
+            edtAge.setText("");
+            edtTarget.setText("");
             Toast.makeText(this, "Form dibersihkan", Toast.LENGTH_SHORT).show();
             return true;
         } else if (id == R.id.mPengaturan) {
@@ -81,4 +128,4 @@ public class ProfileActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
